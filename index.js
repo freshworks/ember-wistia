417d39514b29d6152409fff6c6c1abb1b71b95ef
Thanashyam Raj <thanashyam@gmail.com>
2018-04-17 14:42:32 +0530

    Removing it from index.html
    Signed-off-by: Thanashyam Raj <thanashyam@gmail.com>

--- index.js
+++ index.js
@@ -2,20 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-wistia',
-  contentFor: function(type, config){
-    if (type === 'head'){
-      return '';
-    } else if (type === 'head-footer'){
-      return '';
-    } else if (type === 'body'){
-      return '';
-    } else if (type === 'body-footer'){
-      if (config.environment === 'test') {
-        return '';
-      } else {
-        return '<script src="https://fast.wistia.com/assets/external/E-v1.js"></script>';
-      }
-    }
-  }
+  name: 'ember-wistia'
 };
